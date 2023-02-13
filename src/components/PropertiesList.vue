bd95d22f934296d37695d8d1aca4884f8bda4c5a
snerva <selenenerva@live.it>
2023-02-13 14:06:57 +0100

    add style

--- src/components/PropertiesList.vue
+++ src/components/PropertiesList.vue
@@ -44,40 +44,40 @@ export default {
   <section class="vue-home pt-5">
     <div class="container">
       <template v-if="properties && !loading">
-        <div class="row row-cols-1 row-cols-sm-3 g-5">
+        <div class="row row-cols-1 row-cols-md-4 g-4 mb-4">
           <div class="col" v-for="property in properties.data">
-            <div class="card">
-              <img
-                class="card-image"
-                :src="getImagePath(property.image)"
-                alt=""
-              />
+            <div class="card border-0" style="height:100%">
+              <div class="image overflow-hidden rounded">
+                <router-link :to="{
+                  name: 'single-property',
+                  params: { slug: property.slug },
+                }">
+                  <img class="img-fluid photo-zoom card-image" :src="getImagePath(property.image)" alt="" />
+                </router-link>
+
+              </div>
               <div class="card-body">
-                <h4>Title: {{ property.title }}</h4>
-                <template v-if="property.description">
-                  <p>
+                <h4 class="card-title">{{ property.title }}</h4>
+                <p class="card-text">{{ property.address }}</p>
+                <p class="card-text">{{ property.latitude }} {{ property.longitude }}</p>
+                <!-- <template v-if="property.description">
+                  <p class="card-text">
                     {{ property.description }}
                   </p>
-                  <router-link
-                    :to="{
-                      name: 'single-property',
-                      params: { slug: property.slug },
-                    }"
-                    >Read more</router-link
-                  >
+                  <router-link :to="{
+                    name: 'single-property',
+                    params: { slug: property.slug },
+                  }" class="nav-link text-orange">Read more <font-awesome-icon icon="fa-solid fa-arrow-right" />
+                  </router-link>
                 </template>
-                <div v-else>No description for this project</div>
-              </div>
-              <div class="card-footer">
-                <div class="type">
-                  <strong>Type: </strong>
-                  <span v-if="property.type">
+                <div v-else>No description for this project</div> -->
+                <div class="type my-3">
+                  <strong class="text-orange" v-if="property.type">
                     {{ property.type.name }}
-                  </span>
+                  </strong>
                   <span v-else>no types yet</span>
                 </div>
                 <div class="technologies">
-                  <strong>Amenities: </strong>
                   <template v-if="property.amenities.length > 0">
                     <span v-for="amenity in property.amenities">
                       #{{ amenity.name }}
@@ -86,10 +86,6 @@ export default {
                   <template v-else>
                     <span>No Amenities yet!.</span>
                   </template>
-                </div>
-                <div>
-                  <p>{{ property.latitude }}</p>
-                  <p>{{ property.longitude }}</p>
                 </div>
               </div>
             </div>
@@ -100,17 +96,10 @@ export default {
       <template v-else-if="loading">
         <div class="loading">
           Loading ...
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-hourglass-split"
-            viewBox="0 0 16 16"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-hourglass-split" viewBox="0 0 16 16">
             <path
-              d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"
-            />
+              d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z" />
           </svg>
         </div>
       </template>
--- src/main.js
+++ src/main.js
@@ -12,11 +12,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faStar, faBath, faBed, faAnglesUp, faMagnifyingGlass, faBars, faAnglesDown, faStreetView, faHouse, faDoorOpen } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faBath, faBed, faAnglesUp, faMagnifyingGlass, faBars, faAnglesDown, faStreetView, faHouse, faDoorOpen, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
 
 /* add icons to the library */
-library.add(faStar, faBath, faBed, faAnglesUp, faMagnifyingGlass, faUser, faBars, faAnglesDown, faStreetView, faHouse, faDoorOpen)
+library.add(faStar, faBath, faBed, faAnglesUp, faMagnifyingGlass, faUser, faBars, faAnglesDown, faStreetView, faHouse, faDoorOpen, faArrowRight)
 
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')

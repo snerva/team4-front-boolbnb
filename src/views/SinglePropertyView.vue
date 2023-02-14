8f090524996c8a12a818b12fab3505a2e04def4c
UniversoCaotico1997 <loria.roberto97@gmail.com>
2023-02-14 10:01:00 +0100

    Add form

--- src/views/SinglePropertyView.vue
+++ src/views/SinglePropertyView.vue
@@ -9,10 +9,54 @@ export default {
     components: { AppHeader, AppFooter },
     data() {
         return {
+            state,
             property: null,
             loading: true,
-            state,
+            name: '',
+            email: '',
+            message: '',
+            phone: '',
+            property: '',
+            success: false,
+            errors: {}
         };
+    },
+    methods: {
+        SendForm() {
+
+            this.loading = true
+
+            this.errors = {}
+
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message,
+                phone: this.phone,
+                property: this.property
+            }
+
+            axios.post(`${this.state.api_url}/api/contacts`, data)
+                .then(response => {
+                    console.log(response.data.success);
+                    this.success = response.data.success
+
+                    // Svuota l'input dopo l'invio
+
+                    if (this.success) {
+                        this.name = '';
+                        this.email = '';
+                        this.message = '';
+                        this.phone = '';
+                        this.property = ''
+                    } else {
+                        this.errors = response.data.errors
+                    }
+
+                    this.loading = false
+                })
+
+        }
     },
     mounted() {
         //console.log(this.$route.params.slug);
@@ -102,7 +146,65 @@ export default {
                             </div>
                         </div>
                         <div class="col">
-                            <div class="contact text-center">...CONTACT FORM</div>
+                            <div class="contact text-center">
+                                <div v-if="success" class="alert alert-success text-start" role="alert">
+                                    Messaggio inviato con successo!
+                                </div>
+
+                                <form @submit.prevent="SendForm()"
+                                    class="border shadow p-3 mb-5 bg-body-tertiary rounded">
+
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Name</label>
+                                        <input type="text" name="" id="" v-model="name" class="form-control"
+                                            placeholder="Mario Rossi" aria-describedby="fullNameHelper">
+                                        <small id="fullNameHelper" class="text-muted">Add your full name</small>
+                                        <!-- error -->
+                                        <p v-for="(error) in errors.message">
+                                            {{ error }}
+                                        </p>
+                                    </div>
+
+
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Email</label>
+                                        <input type="email" name="" id="" v-model="email" class="form-control"
+                                            placeholder="mario.rossi@example.com" aria-describedby="emailHelper">
+                                        <small id="emailHelper" class="text-muted">Add your email address</small>
+                                        <!-- error -->
+                                        <p v-for="(error) in errors.message">
+                                            {{ error }}
+                                        </p>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Phone</label>
+                                        <input type="email" name="" id="" v-model="phone" class="form-control"
+                                            placeholder="+39 3271569777" aria-describedby="emailHelper">
+                                        <small id="emailHelper" class="text-muted">Add your Phone Number</small>
+                                        <!-- error -->
+                                        <p v-for="(error) in errors.message">
+                                            {{ error }}
+                                        </p>
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Message</label>
+                                        <textarea class="form-control" name="" id="" v-model="message"
+                                            rows="5"></textarea>
+                                        <!-- error -->
+                                        <p v-for="(error) in errors.message">
+                                            {{ error }}
+                                        </p>
+
+                                    </div>
+
+
+                                    <button type="submit" class="btn btn-primary" :disabled="loading">
+                                        {{ loading? 'Sending...': 'Contact me' }}
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                     <div class="maps">

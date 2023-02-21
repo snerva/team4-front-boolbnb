051d62789b98d827dc4ff29380a04232c054b9d9
sgraziola <simo.fdl@gmail.com>
2023-02-21 17:38:06 +0100

    hide some logs

--- src/views/PropertiesView.vue
+++ src/views/PropertiesView.vue
@@ -66,13 +66,16 @@ export default {
               this.hasSponsorship.push(property);
             } else {
               this.hasNoSponsorship.push(property);
+
             }
           }
+          //console.log(this.hasSponsorship);
+          //console.log(this.hasNoSponsorship);
 
           // svuoto array coordinate degli appartamenti trovati
           this.apartmentsCoordinates = [];
 
-          console.log(this.filteredList);
+          //console.log(this.filteredList);
         })
         .catch((err) => {
           console.error(err)
